d0bbdd103cee1ae943f8a04bd93bb8693a004661
jeongdoc <jeong.dev01@gmail.com>
2023-02-10 09:48:12 +0900

    update

--- _javascript/utils/img-extra.js
+++ _javascript/utils/img-extra.js
@@ -1,46 +1,28 @@
 /**
- Lazy load images (https://github.com/ApoorvSaxena/lozad.js)
- and popup when clicked (https://github.com/dimsemenov/Magnific-Popup)
+ * Set up image stuff
  */
 
- $(function () {
-  const IMG_SCOPE = '#main > div.row:first-child > div:first-child';
+(function() {
+    if ($('#core-wrapper img[data-src]') <= 0) {
+        return;
+    }
 
-  if ($(`${IMG_SCOPE} img`).length <= 0) {
-      return;
-  }
+    /* See: <https://github.com/dimsemenov/Magnific-Popup> */
+    $('.popup').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        showCloseBtn: false,
+        zoom: {
+            enabled: true,
+            duration: 300,
+            easing: 'ease-in-out'
+        }
+    });
 
-  /* lazy loading */
+    /* Stop shimmer when image loaded */
+    document.addEventListener('lazyloaded', function(e) {
+        const $img = $(e.target);
+        $img.parent().removeClass('shimmer');
+    });
 
-  const imgList = document.querySelectorAll(`${IMG_SCOPE} img[data-src]`);
-  const observer = lozad(imgList);
-  observer.observe();
-
-  /* popup */
-
-  $(`${IMG_SCOPE} p > img[data-src], ${IMG_SCOPE} img[data-src].preview-img`).each(
-      function () {
-          let nextTag = $(this).next();
-          const title = nextTag.prop('tagName') === 'EM' ? nextTag.text() : '';
-          const src = $(this).attr('data-src'); // created by lozad.js
-
-          $(this).wrap(`<a href="${src}" title="${title}" class="popup"></a>`);
-      }
-  );
-
-  $('.popup').magnificPopup({
-      type: 'image',
-      closeOnContentClick: true,
-      showCloseBtn: false,
-      zoom: {
-          enabled: true,
-          duration: 300,
-          easing: 'ease-in-out'
-      }
-  });
-
-  /* markup the image links */
-
-  $(`${IMG_SCOPE} a`).has('img').addClass('img-link');
-
-});
+})();
